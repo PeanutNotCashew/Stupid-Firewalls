a752e79f8b70d9feffed6f0a13fd26056bbd96a6
Peanut-windowslaptop <93226333+PeanutNotCashew@users.noreply.github.com>
2024-11-22 17:26:30 -0500

    Upload spy (FreeBSD)

---
+++ Firewall Scripts/spy.sh
@@ -0,0 +1,37 @@
+#! bin/sh
+
+#https://docs.freebsd.org/en/books/handbook/firewalls/
+
+# freeBSD only uses proprietary firewalls
+
+# Enable PF
+if $(! sysrc pf_enable | grep -q "YES"); then
+    sudo sysrc pf_enable="YES"
+fi
+# Double check using correct rule file
+if $(! sysrc pf_rules | grep -q "/etc/pf.conf"); then
+    sudo sysrc pf_rules="/etc/pf.conf"
+fi
+# Check that rule file is created
+if $(! -f /etc/pf.conf); then
+    sudo touch /etc/pf.conf
+    sudo service pf start
+fi
+
+# Enable
+pfctl -e
+
+# Clear rule file
+echo '' | sudo tee /etc/pf.conf
+
+# Add rules
+echo 'block in' | sudo tee -a /etc/pf.conf
+echo 'pass out' | sudo tee -a /etc/pf.conf
+
+echo 'pass in quick to any port { 22, 3306 }' | sudo tee -a /etc/pf.conf
+
+# Flush existing rules, and load newly created rules
+sudo pfctl -F all -f /etc/pf.conf
+
+# Print rules
+sudo pfctl -s rules
