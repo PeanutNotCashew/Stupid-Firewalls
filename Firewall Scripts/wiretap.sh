ab999377229d7a27d2feba757d7946104ff06cb5
Peanut-windowslaptop <93226333+PeanutNotCashew@users.noreply.github.com>
2024-11-22 09:49:23 -0500

    Complete wiretap

--- Firewall Scripts/wiretap.sh
+++ Firewall Scripts/wiretap.sh
@@ -3,12 +3,25 @@
 if ! systemctl list-unit-files --type=service | grep -q "firewallf.service"
 then # Install if not already installed
     dnf install firewalld
-
+    systemctl enable firewalld
 fi
+
+systemctl start firewalld
 
 # Assume the public facing interface is public
 firewall-cmd --set-default-zone=public
 
 # Services
-firewall-cmd --permanent --zone=public --add-port=25 # SMTP
-firewall-cmd --permanent --zone=public --add-port=143 # IMAP
+firewall-cmd --permanent --zone=public --add-port=25/tcp # SMTP
+firewall-cmd --permanent --zone=public --add-port=143/tcp # IMAP
+
+# Reload
+firewall-cmd --reload
+
+# Display rules for manually removing extra rules
+# Only the public zone should be active
+firewall-cmd --list-all
+
+# To remove, run either:
+# firewall-cmd --zone=public --remove-service=[service]
+# firewall-cmd --zone=public --remove-port=[port]/[protocol]
