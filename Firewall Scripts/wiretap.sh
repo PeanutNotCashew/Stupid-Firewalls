bd7f82903cfdd4728f82c29ba6bf3eaa02666db9
Peanut-windowslaptop <93226333+PeanutNotCashew@users.noreply.github.com>
2024-11-22 09:20:06 -0500

    Add installation condition

--- Firewall Scripts/wiretap.sh
+++ Firewall Scripts/wiretap.sh
@@ -1,5 +1,11 @@
 #! bin/bash
 
+if ! systemctl list-unit-files --type=service | grep -q "firewallf.service"
+then # Install if not already installed
+    dnf install firewalld
+
+fi
+
 # Assume the public facing interface is public
 firewall-cmd --set-default-zone=public
 
