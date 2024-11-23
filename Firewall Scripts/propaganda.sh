b42010ad33ad0e5701d1ae4317dc1cca78a67d8e
Peanut-windowslaptop <93226333+PeanutNotCashew@users.noreply.github.com>
2024-11-22 19:46:57 -0500

    Add install check

--- Firewall Scripts/propaganda.sh
+++ Firewall Scripts/propaganda.sh
@@ -1,7 +1,16 @@
 #! bin/bash
 
-ufw default deny
+if ! systemctl list-unit-files --type=service | grep -q "ufw.service"
+then # Install if not already installed
+    sudo apt install ufw
+else # Reset if already installed
+    sudo ufw reset
+fi
+# Turn UFW on
+sudo ufw enable
+
+sudo ufw default deny
 
 # Allow services
-ufw allow 80
-ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 22
